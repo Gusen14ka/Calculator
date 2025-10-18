1f89abfb4969a7135349f9cd36db2a61528c622b
Gusen14ka <faktur2005@yandex.ru>
2025-10-18 03:00:22 +0300

    В PluginManager добавлены функции выгрузки, поиска (из добавленных) плагинов и функция проверка директории на появление новых/изменение добавленных плагинов

--- include/plugin_helpers/PluginManager.hpp
+++ include/plugin_helpers/PluginManager.hpp
@@ -5,9 +5,9 @@
 #include "plugin_helpers/PluginHandle.hpp"
 #include <filesystem>
 #include <memory>
+#include <string>
 #include <unordered_map>
 
-using PluginPtr = std::shared_ptr<PluginHandle>;
 
 class PluginManager{
 public:
@@ -18,13 +18,10 @@ public:
     bool load(std::filesystem::path const & path, std::string* out_err = nullptr);
 
     // Выгрузка плагина по пути
-    bool unload_by_path(std::filesystem::path const & path, std::string* out_err = nullptr);
-
-    // Выгрузка плагина по имени функции
-    bool unload_by_name(std::string const & path, std::string* out_err = nullptr);
+    bool unload_by_path(std::string const & path, std::string* out_err = nullptr);
 
     // Поиск по имени функции. Возвращаем shared_ptr чтобы обеспечить длительность жизни
-    std::shared_ptr<PluginHandle> find(std::string const & name);
+    std::shared_ptr<PluginHandle> find(std::string const & name) const;
 
     // Проверяет папку с плагинами, если есть изменения - загружает
     void scan_directory(std::filesystem::path const & dir);
@@ -33,12 +30,27 @@ private:
     bool verify_and_init_plugin(std::shared_ptr<PluginHandle> p, std::string* out_err);
     void finish_unload(std::shared_ptr<PluginHandle> p); // shutdown + dlclose
 
+    // Выгрузка плагина по имени функции
+    bool unload_by_name(std::string const & path, std::string* out_err = nullptr);
+
     // Убираем пробелы с концов и приводим к нижнему регистру
-    std::string normalize_name(std::string s);
+    std::string normalize_name(std::string s) const;
+
+    // Приводим строку к нижнему регистру
+    std::string to_lower_str(std::string s) const;
+
+    // Проверка расширения файла
+    bool has_lib_extension(std::filesystem::path const & path) const;
+
+    // Удаляем все бекапы
+    void cleanup_temp_backups() noexcept;
 
     HostApi const * host_ = nullptr;
     
     // Основыным контейнером является by_path_, тк в by_name_ также зарегестрированы элиасы функций
     std::unordered_map<std::string, std::shared_ptr<PluginHandle>> by_name_;
     std::unordered_map<std::string, std::shared_ptr<PluginHandle>> by_path_;
+
+    // Контейнер соответсвия между изначальным путём к плагину и к его временному бекапу
+    std::unordered_map<std::string, std::filesystem::path> temp_backups_;
 };
--- src/plugin_helpers/PluginManager.cpp
+++ src/plugin_helpers/PluginManager.cpp
@@ -2,10 +2,14 @@
 #include "plugin_helpers/PluginAPI.h"
 #include "plugin_helpers/PluginHandle.hpp"
 #include "plugin_helpers/platform.hpp"
+#include <cstddef>
 #include <exception>
 #include <filesystem>
 #include <memory>
+#include <string>
+#include <system_error>
 #include <vector>
+#include <chrono>
 
 
 PluginManager::~PluginManager() noexcept{
@@ -19,6 +23,7 @@ PluginManager::~PluginManager() noexcept{
     }
     by_name_.clear();
     by_path_.clear();
+    cleanup_temp_backups();
 }
 
 bool PluginManager::load(std::filesystem::path const & path, std::string* out_err){
@@ -154,25 +159,23 @@ bool PluginManager::load(std::filesystem::path const & path, std::string* out_er
             return false;
         }
         by_name_[norm_name] = plug_handle;
-        by_path_[path.string()] = plug_handle;
     }
+    by_path_[path.string()] = plug_handle;
 
     // Вызовем инициализатор плагина, если такой есть
-    if(plug_handle->init){
-        int res = plug_handle->do_init(host_, out_err);
-        if(res != 0){
-            by_path_.erase(path.string());
-            plug_handle->close_library();
-            //TODO:
-            //LOG.error
-            return false;
-        }
+    res = plug_handle->do_init(host_, out_err);
+    if(res != 0){
+        by_path_.erase(path.string());
+        plug_handle->close_library();
+        //TODO:
+        //LOG.error
+        return false;
     }
-    //TODO: LOG.info
+//TODO: LOG.info
     return true;
 }
 
-std::string PluginManager::normalize_name(std::string s){
+std::string PluginManager::normalize_name(std::string s) const{
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c){
         return !std::isspace(c);
     }));
@@ -185,4 +188,200 @@ std::string PluginManager::normalize_name(std::string s){
     });
 
     return s;
+}
+bool PluginManager::unload_by_path(std::string const & path, std::string* out_err){
+    auto it = by_path_.find(path);
+    if(it == by_path_.end()){
+        if(out_err) *out_err = "not found in map: " + path;
+        //TODO:
+        // LOG.error
+        return false;
+    }
+    bool res_by_name = true;
+    auto plugin_handle = it->second;
+    for(auto& el: plugin_handle->names){
+        res_by_name = unload_by_name(el, out_err) ? res_by_name : false;
+    }
+    // TODO:
+    // Решить что делать в случае ошибке удаления по псевдониму: удалять по пути или нет?
+    // Пока что принято решение - в любом случае удалять по пути
+    by_path_.erase(it);
+
+    plugin_handle->do_shutdown();
+    plugin_handle->close_library();
+    //TODO: LOG.info
+    return true;
+}
+
+bool PluginManager::unload_by_name(std::string const & name, std::string * out_err){
+    std::string norm_name = normalize_name(name);
+    auto it = by_name_.find(norm_name);
+    if(it == by_name_.end()){
+        if(out_err) *out_err = "not found by name: " + name;
+        // TODO:
+        // LOG.error
+        return false;
+    }
+    by_name_.erase(it);
+    // TODO:
+    // LOG.info
+    return true;
+}
+
+std::shared_ptr<PluginHandle> PluginManager::find(std::string const& name) const{
+    auto it = by_name_.find(normalize_name(name));
+    if(it == by_name_.end()){
+        return nullptr;
+    }
+    return it->second;
+}
+
+std::string PluginManager::to_lower_str(std::string s) const{
+    std::transform(s.begin(), s.end(), s.begin(),
+        [](unsigned char c){ return std::tolower(c); });
+    return s;
+}
+
+bool PluginManager::has_lib_extension(std::filesystem::path const & path) const{
+    auto fname = path.filename().string();
+    if(fname.empty()) return false;
+    if (fname.find('.') != fname.npos) return false;                             // .hidden
+    if (fname.find('~') != fname.npos) return false;               // backup/temp with ~
+    if (fname.find(".tmp") != fname.npos) return false;            // .tmp
+    if (fname.find(".part") != fname.npos) return false;
+
+    #ifdef _WIN32
+        auto ext = to_lower_str(path.extension().string());
+        return ext == ".dll";
+    #else
+        auto ext = to_lower_str(path.extension().string());
+        return ext == ".so";
+    #endif
+}
+
+void PluginManager::scan_directory(std::filesystem::path const & dir){
+    std::error_code ec;
+    for(auto& entry: std::filesystem::directory_iterator(dir, ec)){
+        if(ec){
+            //TODO LOG.error
+            break;
+        }
+        if(!entry.is_regular_file(ec) || ec){
+            //TODO LOG.error
+            continue;
+        }
+
+        auto path = entry.path();
+
+        if(!has_lib_extension(path)){
+            //TOGO LOG
+            continue;
+        }
+
+        auto new_time = std::filesystem::last_write_time(path, ec);
+        if(ec){
+            //TODO LOG.error
+            continue;
+        }
+
+        // Проверяем добавлен ли уже этот плагин, если нет - добавляем
+        // Если да, перегружаем на новую версию 
+        auto it = by_path_.find(path.string());
+        if(it == by_path_.end()){
+            std::string err;
+            auto res = load(path, &err);
+            if(!res){
+                //TODO LOG.error
+                continue;
+            }
+        }
+
+        // Проверяем на изменение файла
+        auto plugin_handle = it->second;
+        auto diff = new_time > plugin_handle->last_write ?
+            new_time - plugin_handle->last_write : plugin_handle->last_write - new_time;
+        if(diff <= std::chrono::seconds(1)){
+            //TODO: LOG.info
+            continue;
+        }
+
+        // Файл изменён - перезаписываем
+        /*
+        Скорее всего чтобы реально безопасно перезаписывать нужно
+        1. Выгрузить во временный файл уже загруженный dll
+        2. unload_by_path() - удаляем изначальный длл
+        3. load() - загружаем новый
+        4. Если неудачно - выгружаем новый
+        5. Загружаем изначальный из временного файла
+        4.2 Если удачно - удаляем временный файл
+        6. Поддержать удаление всех временных файлов при завершении приложения
+        */
+        std::filesystem::path tmp_path = std::filesystem::temp_directory_path() / (path.filename().string() + ".backup.");
+        
+        // Создадим уникальное имя
+        unsigned idx = 0;
+        std::filesystem::path tmp_path_candidate;
+        do{
+            tmp_path_candidate = tmp_path.string() + std::to_string(idx++) + ".tmp";
+        }while(std::filesystem::exists(tmp_path_candidate));
+        tmp_path = tmp_path_candidate;
+
+        // Бекапаем оригинал
+        if(!std::filesystem::copy_file(path, tmp_path, std::filesystem::copy_options::none, ec) || ec){
+            //TODO
+            // LOG.error
+            continue;
+        }
+        temp_backups_[path.string()] = tmp_path;
+
+        // Выгружаем оригнал 
+        std::string err;
+        if(!unload_by_path(path.string(), &err)){
+            // TODO: LOG
+            // Пока что мы просто создали бекап - можем спокойно скипнуть
+            continue;
+        }
+        
+        // Загружаем новую версию
+        err = "";
+        if(load(path, &err)){
+            // Успех => удаляем бекап
+            std::filesystem::remove(tmp_path, ec);
+            temp_backups_.erase(path.string());
+            //TODO: LOG.info
+            continue;
+        }
+
+        // Неуспех => перезаписываем - вместо новой бекап
+        std::filesystem::copy_file(tmp_path, path, std::filesystem::copy_options::overwrite_existing, ec);
+        if(!ec){
+            if(load(path, &err)){
+                std::filesystem::remove(tmp_path, ec);
+                if(ec){
+                    //TODO: LOG.error
+                    continue;
+                }
+                temp_backups_.erase(path.string());
+                //TODO: LOG.info
+                continue;
+            }
+            else{
+                // TODO: LOG.error
+                continue;
+            }
+        }
+        else{
+            // TODO: LOG.error
+            // Если будут проблемы можно попробовать загружать плагин из временного файла без копирования
+            continue;
+        }
+    }
+}
+
+void PluginManager::cleanup_temp_backups() noexcept{
+    for(auto& tmp : temp_backups_){
+        std::error_code ec;
+        std::filesystem::remove(tmp.second, ec);
+    }
+    temp_backups_.clear();
 }
